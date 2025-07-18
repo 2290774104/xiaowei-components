7cf9bb6ebd57d411907f7ea5c5a5c7cc754c37e5
tanghc <tanghc@xweisoft.com>
2025-07-18 09:04:41 +0800

    feat: 封装http模块

---
+++ http/index.ts
@@ -0,0 +1,31 @@
+import {
+  $post,
+  $get,
+  $delete,
+  $put,
+  $getBlob,
+  $postBlob,
+  $postArraybuffer,
+} from './http';
+
+export {
+  $post,
+  $get,
+  $delete,
+  $put,
+  $getBlob,
+  $postBlob,
+  $postArraybuffer,
+};
+
+const http = {
+  $post,
+  $get,
+  $delete,
+  $put,
+  $getBlob,
+  $postBlob,
+  $postArraybuffer,
+}
+
+export default http
